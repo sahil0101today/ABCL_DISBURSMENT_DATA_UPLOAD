3b48f7935d672910074751ecb56165daf9cc5f55
Sahil Thombare <122919311+sahil0101today@users.noreply.github.com>
2026-04-21 07:59:05 +0530

    Update daataupload.py

--- daataupload.py
+++ daataupload.py
@@ -5,9 +5,34 @@ Created on Mon Apr 20 14:47:57 2026
 @author: sahil_pc
 """
 
-import base64
 import os
+import json
+import base64
+import numpy as np
 import pandas as pd
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+
+encoded_key = os.environ.get("GCP_SA_KEY")
+decoded_key = json.loads(base64.b64decode(encoded_key))
+
+credentials = service_account.Credentials.from_service_account_info(decoded_key)
+
+client = bigquery.Client(
+    credentials=credentials,
+    project="bigqueryfacebook"
+)
+
+table_id = "bigqueryfacebook.ABCL.ABCL_DISBURSMENT_DATA"
+
+query = f"""
+SELECT *
+FROM `{table_id}`
+"""
+df1 = client.query(query).to_dataframe()
+
+###########################################################################
 
 # Read inputs from environment variables
 file1_base64 = os.getenv("FILE1")
@@ -22,17 +47,28 @@ save_file(file1_base64, "file1.csv")
 save_file(file2_base64, "file2.csv")
 
 # Read CSVs
-df1 = pd.read_csv("file1.csv")
-df2 = pd.read_csv("file2.csv")
+df2 = pd.read_csv("file1.csv")
+df3 = pd.read_csv("file2.csv")
 
-print("File1 Head:")
-print(df1.head())
+###############################################################################
 
-print("File2 Head:")
-print(df2.head())
+table_id = "bigqueryfacebook.ABCL.ABCL_MOE_DATA"
 
-# Sample processing
-merged = pd.concat([df1, df2])
-merged.to_csv("output.csv", index=False)
+job_config = bigquery.LoadJobConfig(
+    schema=schema,
+    write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
+    autodetect=False
+)
+# -----------------------------
+# 5. Upload
+# -----------------------------
+job = client.load_table_from_dataframe(
+    df3,
+    table_id,
+    job_config=job_config
+)
 
-print("Processing Completed!")
+job.result()
+print("✅ Data appended successfully to BigQuery 🚀")
+
+###############################################################################
